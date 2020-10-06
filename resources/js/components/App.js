912b831da3d2dc2c718a60bd48b4f368cc1b8227
vladimirvirijevic <vladanfs96@gmail.com>
2020-10-06 10:06:11 +0200

    Set up Ant Desing

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter } from "react-router-dom";
 import Test from "./Test";
+import "../../css/App.css";
+import { Button } from "antd";
 
 function App() {
     return (
         <div className="container">
             <h1>App</h1>
+            <Button type="primary">Button</Button>
             <Route path="/test" component={Test} />
         </div>
     );
